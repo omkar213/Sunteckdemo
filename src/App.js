7af5b64477272661de4171ed039e269773b590b3
omkar213 <omkardhumal213@gmail.com>
2022-02-10 13:02:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Body from '../src/Components/Body';
+import Amenities from '../src/Components/Amenities';
+import Location from './Components/Location';
+import Contactus from './Components/Contactus';
+import Gallery from './Components/Gallery';
+import Floorplan from './Components/Floorplan';
+import Testimonals from './Components/Testimonals';
+import Disclaimer from './Components/Disclaimer';
+
+const App = () => {
+    return(
+      <>
+        <div>
+            <Header/>
+            <Body/>
+            <Amenities/>
+            <Location/>
+            <Contactus/>
+            <Gallery/>
+            <Floorplan/>
+            <Testimonals/>
+            <Disclaimer/>
+        </div>
+      </>
+    )   
+}
+
+export default App;
